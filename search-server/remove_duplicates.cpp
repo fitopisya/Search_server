7a329e9f6808ebfc29cb9f4d08f8afeff154cede
fitolearn <112857538+fitolearn@users.noreply.github.com>
2023-03-02 14:03:14 +0300

    Update remove_duplicates.cpp

--- search-server/remove_duplicates.cpp
+++ search-server/remove_duplicates.cpp
@@ -1,24 +1,22 @@
 #include "remove_duplicates.h"
 
-void RemoveDuplicates(SearchServer& search_server) {
-    std::map<std::vector<std::string>, int> doc_words_with_id;
-    std::vector<int> duplicate_ids;
-
-    for (const int doc_id : search_server) {
-        const auto& word_freq = search_server.GetWordFrequencies(doc_id);
-        std::vector<std::string> words(word_freq.size());
-        std::transform(word_freq.begin(), word_freq.end(),std::inserter(words, words.begin()),
-                       [](const std::pair<std::string, double>& elements) {
-                    return elements.first;
-        });
-
-        auto [word, no_match] = doc_words_with_id.emplace(words, doc_id);
-        if (!no_match) {
-            duplicate_ids.push_back(doc_id);
+void RemoveDuplicates(SearchServer& search_server) noexcept {
+    std::set<std::vector<std::string>> words;
+    std::vector<int> ids_to_delete;
+    for (const int doc : search_server) {
+        std::vector<std::string> vec_with_words;
+        for (const auto& [key, val] : search_server.GetWordFrequencies(doc)) {
+            vec_with_words.push_back(key);
+        }
+        if (words.find(vec_with_words) == words.end()) {
+            words.insert(vec_with_words);
+        }
+        else {
+            ids_to_delete.push_back(doc);
         }
     }
-    for (const int doc_id : duplicate_ids) {
-        std::cout << "Found duplicate document id "s << doc_id << "\n";
-        search_server.RemoveDocument(doc_id);
+    for (const auto id : ids_to_delete) {
+        std::cout << "Found duplicate document id " << id << "\n";
+        search_server.RemoveDocument(id);
     }
 }
